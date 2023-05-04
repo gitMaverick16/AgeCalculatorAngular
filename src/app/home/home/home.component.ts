2d7454c536d57ebf397731368b3012e97a0922eb
Sergio Rondon <sergio19980816@gmail.com>
2023-05-03 23:12:28 -0500

    Adding html and css

---
+++ src/app/home/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  public myForm: FormGroup = this.fb.group({
+    day: ['', [ Validators.required, Validators.minLength(3) ] ],
+    month: [0, [ Validators.required, Validators.min(0) ] ],
+    year: [0, [ Validators.required, Validators.min(0) ]],
+  })
+
+  constructor( private fb: FormBuilder ){}
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave():void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+
+  }
+}
